f3db07442017b335074cafe39ab9d684d99bee46
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2022-04-22 12:41:47 +0800

    fix: redirect when already logged in

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::get('/login', [LoginController::class, 'index'])->middleware('guest')->name('login');
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::post('/logout', LogoutController::class)->name('logout');
 
