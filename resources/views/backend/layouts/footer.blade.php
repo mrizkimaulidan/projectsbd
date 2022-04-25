fb0e1953b8a37a8e83be3071e037e31eb7158edc
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2022-04-25 11:28:10 +0800

    add: footer application name

--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
   <!-- To the right -->
   <div class="float-right d-none d-sm-inline">
-    Anything you want
+    {{ config('app.name') }}
   </div>
   <!-- Default to the left -->
   <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
