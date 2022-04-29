8473fda097ccb305e6bd010dc93b3dbafad5a05d
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2022-04-30 04:51:07 +0800

    feat: change articles error alert

--- resources/views/backend/articles/create.blade.php
+++ resources/views/backend/articles/create.blade.php
@@ -5,15 +5,17 @@
   <div class="col-lg-12">
     <div class="card">
       <div class="card-body">
-        @include('utilities.alert')
         <form action="{{ route('backend.articles.store') }}" method="POST" enctype="multipart/form-data">
           @csrf
           <div class="row">
             <div class="col-lg-6">
               <div class="mb-3">
                 <label for="title" class="form-label">Judul :</label>
-                <input type="text" name="title" id="title" value="{{ old('title') }}" class="form-control"
-                  placeholder="Masukkan judul.." autofocus>
+                <input type="text" name="title" id="title" value="{{ old('title') }}"
+                  class="form-control @error('title') is-invalid @enderror" placeholder="Masukkan judul.." autofocus>
+                @error('title')
+                <small class="text-danger">{{ $errors->first('title') }}</small>
+                @enderror
               </div>
             </div>
             <div class="col-lg-6">
@@ -29,11 +31,14 @@
             <div class="col-lg-6">
               <div class="mb-3">
                 <label for="is_active">Aktif/Tidak Aktif :</label>
-                <select name="is_active" id="is_active" class="form-select">
+                <select name="is_active" id="is_active" class="form-select @error('is_active') is-invalid @enderror">
                   <option value="">Pilih..</option>
-                  <option value="1" @selected(old('is_active')==1)>Aktif</option>
-                  <option value="0" @selected(old('is_active')==0)>Tidak Aktif</option>
+                  <option value="1">Aktif</option>
+                  <option value="0">Tidak Aktif</option>
                 </select>
+                @error('is_active')
+                <small class="text-danger">{{ $errors->first('is_active') }}</small>
+                @enderror
               </div>
             </div>
 
@@ -49,16 +54,23 @@
 
           <div class="mb-3">
             <label for="thumbnail" class="form-label">Pilih Gambar :</label>
-            <input type="file" name="thumbnail" id="thumbnail" class="form-control"
+            <input type="file" name="thumbnail" id="thumbnail"
+              class="form-control @error('thumbnail') is-invalid @enderror"
               oninput="image_preview.src=window.URL.createObjectURL(this.files[0])">
+            @error('thumbnail')
+            <small class="text-danger">{{ $errors->first('thumbnail') }}</small><br>
+            @enderror
             <small class="text-muted">*Ekstensi yang diperbolehkan jpg/png/jpeg</small><br>
             <small class="text-muted">*Ukuran gambar maksimal 2mb!</small>
           </div>
 
           <div class="mb-3">
             <label for="body" class="form-label">Konten :</label>
-            <textarea name="body" id="body" rows="5" class="form-control"
-              placeholder="Masukkan konten..">{{ old('body') }}</textarea>
+            <textarea name="body" id="body" rows="5"
+              class="form-control @error('body') is-invalid @enderror">{{ old('body') }}</textarea>
+            @error('body')
+            <small class="text-danger">{{ $errors->first('body') }}</small>
+            @enderror
           </div>
           <a href="{{ route('backend.articles.index') }}" class="btn btn-secondary">Kembali</a>
           <button type="submit" class="btn btn-primary">Tambah</button>
--- resources/views/backend/articles/edit.blade.php
+++ resources/views/backend/articles/edit.blade.php
@@ -5,25 +5,30 @@
   <div class="col-lg-12">
     <div class="card">
       <div class="card-body">
-        @include('utilities.alert')
         <form action="{{ route('backend.articles.update', $article) }}" method="POST" enctype="multipart/form-data">
           @csrf
           @method('PUT')
           <div class="mb-3">
             <label for="title" class="form-label">Judul :</label>
-            <input type="text" name="title" id="title" class="form-control" placeholder="Masukkan judul.."
-              value="{{ $article->title }}" autofocus>
+            <input type="text" name="title" id="title" class="form-control @error('title') is-invalid @enderror"
+              placeholder="Masukkan judul.." value="{{ $article->title }}" autofocus>
+            @error('title')
+            <small class="text-danger">{{ $errors->first('title') }}</small>
+            @enderror
           </div>
 
           <div class="row">
             <div class="col-lg-6">
               <div class="mb-3">
                 <label for="is_active">Aktif/Tidak Aktif :</label>
-                <select name="is_active" id="is_active" class="form-select">
-                  <option selected>Pilih..</option>
+                <select name="is_active" id="is_active" class="form-select @error('is_active') is-invalid @enderror">
+                  <option value="" selected>Pilih..</option>
                   <option value="1" {{ $article->is_active === 1 ? 'selected' : '' }}>Aktif</option>
                   <option value="0" {{ $article->is_active === 0 ? 'selected' : '' }}>Tidak Aktif</option>
                 </select>
+                @error('is_active')
+                <small class="text-danger">{{ $errors->first('is_active') }}</small>
+                @enderror
               </div>
             </div>
 
@@ -42,16 +47,23 @@
 
           <div class="mb-3">
             <label for="thumbnail" class="form-label">Pilih Gambar :</label>
-            <input type="file" name="thumbnail" id="thumbnail" class="form-control"
+            <input type="file" name="thumbnail" id="thumbnail"
+              class="form-control @error('thumbnail') is-invalid @enderror"
               oninput="image_preview.src=window.URL.createObjectURL(this.files[0])">
+            @error('thumbnail')
+            <small class="text-danger">{{ $errors->first('thumbnail') }}</small><br>
+            @enderror
             <small class="text-muted">*Ekstensi yang diperbolehkan jpg/png/jpeg</small><br>
             <small class="text-muted">*Ukuran gambar maksimal 2mb!</small>
           </div>
 
           <div class="mb-3">
             <label for="body" class="form-label">Konten :</label>
-            <textarea name="body" id="body" rows="5" class="form-control"
+            <textarea name="body" id="body" rows="5" class="form-control @error('body') is-invalid @enderror"
               placeholder="Masukkan konten..">{{ $article->body }}</textarea>
+            @error('body')
+            <small class="text-danger">{{ $errors->first('body') }}</small>
+            @enderror
           </div>
           <a href="{{ route('backend.articles.index') }}" class="btn btn-secondary">Kembali</a>
           <button type="submit" class="btn btn-primary">Ubah</button>
